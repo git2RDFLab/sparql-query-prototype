e7f8e75f0f330d23cdab688377b3fffad2dafd8c
Superdose <72471357+Superdose@users.noreply.github.com>
2024-03-03 14:59:33 +0100

    Disable tests

--- src/test/java/de/leipzig/htwk/gitrdf/sparql/query/QueryApplicationTests.java
+++ src/test/java/de/leipzig/htwk/gitrdf/sparql/query/QueryApplicationTests.java
@@ -1,9 +1,11 @@
 package de.leipzig.htwk.gitrdf.sparql.query;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class QueryApplicationTests {
 
 	@Test
